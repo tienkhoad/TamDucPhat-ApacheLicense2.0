cdeaa672d80c7c82b82b3fe38c302189bd7cf3df
Dinh Tien Khoa (KhoaDT) <tienkhoad@gmail.com>
2023-12-05 07:27:35 +0700

    fix: modify slide

---
+++ TamDucPhat.reactjs/tamducphat-fe/src/style/ProductsScreen.js
@@ -0,0 +1,8 @@
+import { Carsousel as CarsouselSlide } from "antd";
+import styled from "styled-components";
+
+export const SliderShowProduct = styled(CarsouselSlide)`
+  .slick-dots {
+    margin: 0 0 0 0 !important;
+  }
+`;
